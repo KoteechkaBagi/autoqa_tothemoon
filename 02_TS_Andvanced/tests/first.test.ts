0c5caf50f03962cff9874c84b8d4d6d1261067a8
VladSelikh <vladikh2405@gmail.com>
2023-02-10 21:28:58 +0300

    feat: add First Array task

---
+++ 02_TS_Andvanced/tests/first.test.ts
@@ -0,0 +1,9 @@
+import type { Equal, Expect } from '../assertions';
+import type { First } from '../tasks/first';
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>,
+]
