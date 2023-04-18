fc376d04db5d6871eed582c3c94b49893ba048bf
KoteechkaBagi <124789704+KoteechkaBagi@users.noreply.github.com>
2023-04-18 19:23:40 +0300

    Builder (#8)
    * Builder
    
    * Added validations for user's mistakes
    
    * Changed if condition

---
+++ 05_TS_builder/string-builder.ts
@@ -0,0 +1,48 @@
+class Builder {
+    build() {
+        throw new Error('Never use super.build()');
+    }
+}
+
+export class StringBuilder extends Builder {
+    constructor(private resultString: string = '') {
+        super();   
+    }
+
+    public append(str: string) : this {
+        this.resultString += str
+        return this
+    }
+
+    public insert(index : number, str : string) : this {
+        let arr = this.resultString.split('');
+        if(index < arr.length) {
+            arr.splice(index + 1, 0, str);
+            this.resultString = arr.join('');
+        } else {
+            throw new Error('You entered incorrect index')
+        }
+        return this
+    }
+
+    public delete(from : number, to : number) : this {
+        let arr = this.resultString.split('');
+        if(to >= from && to < arr.length) {
+            arr.splice(from, (to - from + 1));
+            this.resultString = arr.join('');
+        } else {
+            throw new Error('You entered incorrect index')
+        }
+        return this
+    }
+
+    public reset() : this {
+        this.resultString = ''
+        return this
+    }
+
+    public build() : string {
+        return this.resultString
+    }
+}
+
